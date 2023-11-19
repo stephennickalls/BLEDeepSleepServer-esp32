056b4c52869fa6b3f2759e9de557b07875243c4d
snickalls <stephennickalls@gmail.com>
2023-11-19 21:40:42 +1300

    Add read fro dht

--- BLEDeepSleepServer-esp32.ino
+++ BLEDeepSleepServer-esp32.ino
@@ -2,21 +2,27 @@
 #include <BLEUtils.h>
 #include <BLEServer.h>
 #include <esp_sleep.h>
+#include <DHT.h>
 
 #define SERVICE_UUID        "12345678-1234-5678-1234-56789abcdef0"
 #define CHARACTERISTIC_UUID "abcdef01-2345-6789-1234-56789abcdef0"
 
 BLECharacteristic *pCharacteristic;
-const uint64_t broadcastInterval = 30000000; // Broadcast for 30 seconds (30,000,000 microseconds)
-const uint64_t sleepInterval = 30000000;     // Sleep for 30 seconds (30,000,000 microseconds)
+const uint64_t broadcastInterval = 60000000; 
+const uint64_t sleepInterval = 60000000;   
+
+#define DHTPIN 4     // Change this to the pin you connected the DHT sensor to
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(115200);
-  
+  dht.begin();
+
   // Check the wake-up reason
   esp_sleep_wakeup_cause_t wakeup_reason;
   wakeup_reason = esp_sleep_get_wakeup_cause();
-  
+
   BLEDevice::init("ESP32_BLE_Server");
   BLEServer *pServer = BLEDevice::createServer();
   BLEService *pService = pServer->createService(SERVICE_UUID);
@@ -26,40 +32,40 @@ void setup() {
                        BLECharacteristic::PROPERTY_WRITE
                      );
 
-  pCharacteristic->setValue("Hello World");
-  pService->start();
+  // Wake up and read data from the DHT sensor
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
 
-  if (wakeup_reason == ESP_SLEEP_WAKEUP_TIMER) {
-    Serial.println("Wakeup caused by timer, start broadcasting...");
-    // Start broadcasting again
-    BLEAdvertising *pAdvertising = pServer->getAdvertising();
-    pAdvertising->start();
-
-    // Set a timer to stop advertising after 30 seconds
-    esp_sleep_enable_timer_wakeup(broadcastInterval);
-    esp_light_sleep_start(); // Use light sleep to keep the program state
-
-    Serial.println("Stop broadcasting, going to deep sleep...");
-    // Now go to deep sleep for 30 seconds
-    esp_sleep_enable_timer_wakeup(sleepInterval);
-    esp_deep_sleep_start();
-  } else {
-    Serial.println("Fresh start or non-timer wakeup");
-    // Fresh start, start advertising
-    BLEAdvertising *pAdvertising = pServer->getAdvertising();
-    pAdvertising->start();
-
-    // Set a timer to stop advertising after 30 seconds
-    esp_sleep_enable_timer_wakeup(broadcastInterval);
-    esp_light_sleep_start(); // Use light sleep to keep the program state
-
-    Serial.println("Stop broadcasting, going to deep sleep...");
-    // Go to deep sleep for 30 seconds
-    esp_sleep_enable_timer_wakeup(sleepInterval);
-    esp_deep_sleep_start();
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(humidity) || isnan(temperature)) {
+      Serial.println("Failed to read from DHT sensor!");
+      // Go to deep sleep again
+      esp_sleep_enable_timer_wakeup(sleepInterval);
+      esp_deep_sleep_start();
   }
+
+  // Create a data string that combines temperature and humidity
+  char sensorData[50];
+  snprintf(sensorData, sizeof(sensorData), "Temp: %.1f C, Hum: %.1f %%", temperature, humidity);
+  pCharacteristic->setValue(sensorData);
+
+  pService->start();
+  Serial.println("Sensor data read, start broadcasting...");
+
+  // Start broadcasting
+  BLEAdvertising *pAdvertising = pServer->getAdvertising();
+  pAdvertising->start();
+
+  // Set a timer to stop advertising after 30 seconds
+  esp_sleep_enable_timer_wakeup(broadcastInterval);
+  esp_light_sleep_start(); // Use light sleep to keep the program state
+
+  Serial.println("Stop broadcasting, going to deep sleep...");
+  // Go to deep sleep for 9 minutes and 30 seconds
+  esp_sleep_enable_timer_wakeup(sleepInterval);
+  esp_deep_sleep_start();
 }
 
 void loop() {
-  // Empty, as the ESP32 will not run the loop after waking up from deep sleep
+
 }
